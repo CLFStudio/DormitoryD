8a0cbd251b88e66d8df4fbb4b6547694c9edc93f
MKFMIKU <mikumkf@outlook.com>
2016-07-21 22:37:04 +0800

    Fix data bug

--- index.js
+++ index.js
@@ -3,51 +3,48 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     path = require('path'),
     mysql = require('mysql'),
-    mongoose = require('mongoose'),
-    db = mongoose.createConnection('localhost','data'),
-    _pool = mysql.createPool({
-        host: 'localhost',
-        port: '3306',
-        user: 'laber',
-        password: 'laber',
-        database: 'lab'
-    });
+    MongoClient = require('mongodb').MongoClient,
+    assert = require('assert'),
+    config = require('./config');
 
-
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname+'/public')));
+app.use(express.static(path.join(__dirname + '/public')));
 
-app.post('/ans', function (req, res) {
-    _pool.getConnection(function (err, conn) {
+app.post('/ans', function(req, res) {
+    _pool.getConnection(function(err, conn) {
         if (!err) {
-            var data = [req.body['0'],req.body['1'],req.body['2'],req.body['3']];
-            conn.query('INSERT INTO `lab`.`datas` (`0`, `1`, `2`, `3`) VALUES (?,?,?,?)',data,function(err){
-                if(!err){
+            var data = [req.body['0'], req.body['1'], req.body['2'], req.body['3']];
+            conn.query('INSERT INTO `lab`.`datas` (`0`, `1`, `2`, `3`) VALUES (?,?,?,?)', data, function(err) {
+                if (!err) {
                     res.end('1');
                     conn.release();
-                }else{
+                } else {
                     console.log(err);
                 }
             });
-        }else{
+        } else {
             console.log(err);
         }
     });
 });
 
-app.post('/ques', function(req, res){
-    var Questions = new mongoose.Schema({
-        questions:[]
+app.post('/ques', function(req, res) {
+    var data = req.body;
+    MongoClient.connect(config.mongodbUrl, function(err, db) {
+        assert.equal(null, err);
+        var collection = db.collection('questions');
+        collection.insert(data,function(err,results){
+            if(!err) res.end('1');
+            else res.end(err);
+        })
     });
-    var QuestionsModel = db.model('questions',Questions);
-    var questions = new QuestionsModel({questions:req.body});
-    questions.save();
-    res.end('1');
 });
 
-var server = app.listen(5000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('Listening at http://%s:%s', host, port);
+var server = app.listen(5000, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Listening at http://%s:%s', host, port);
 });
--- public/js/admin.js
+++ public/js/admin.js
@@ -1,24 +1,33 @@
 Vue.http.options.emulateJSON = true;
+Vue.config.devtools = true;
 
 var ques = new Vue({
     el: '#questions',
     data:{
-        quesData: {questions:[]}
+        quesData: [],
+        tempAns: ''
     },
     methods: {
         addAns: function (index) {
             if(this.tempAns){
-                this.quesData.questions[index].ans.push(this.tempAns);
+                this.quesData[index].ans.push(this.tempAns);
                 this.tempAns="";
             }
         },
         addQues: function () {
             var tQues={question:'',ans:[]};
-            this.quesData.questions.push(tQues);
+            this.quesData.push(tQues);
         },
         save: function () {
-            this.$http.post('/ques', this.quesData).then(function(res) {
-			    if(res.body==1) alert("OK");
+            var quesData=this.quesData;
+            console.log(quesData);
+            var quesDataNow={
+                time:Date(),
+                data:quesData
+            }
+            quesDataNow = JSON.stringify(quesDataNow);
+            this.$http.post('/ques', quesDataNow).then(function(res) {
+			    if(res.body==0) alert("提交OK");
                 else alert(res);
 			});
         }
